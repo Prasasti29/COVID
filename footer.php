0c48faf9025e413e40d19daa587f81c40dc6b038
Prasasti29 <62132067+Prasasti29@users.noreply.github.com>
2021-04-04 22:45:12 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+
+$error = false;
+if($error = false)
+{
+    //the beautifully styled content, everything looks good
+    echo '<div id="content">some text</div>';
+}
+else
+{
+    //bad looking, unstyled error :-( 
+} 
+?>
